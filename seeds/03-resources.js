b143cd4c69c55ded91b44be37117a46c6bc973a5
gspayonk <grissobel.castro@gmail.com>
2019-11-08 15:25:07 -0500

    database error

--- seeds/03-resources.js
+++ seeds/03-resources.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('resources').insert([
-        {resource_name: 'shampoo, tub, dog', description: 'cleaning supplies'},
-        {resource_name: 'kitchen, ingredients, instructions', description: 'cooking supplies'},
-        {resource_name: 'book, lecture material, homework instructions', description: 'homework related materials'}
+        {name: 'shampoo, tub, dog', description: 'cleaning supplies'},
+        {name: 'kitchen, ingredients, instructions', description: 'cooking supplies'},
+        {name: 'book, lecture material, homework instructions', description: 'homework related materials'}
       ]);
     });
 };
